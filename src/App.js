7fa84fd3fbc49d8ab9e8c2330cdd52f33179635f
SwethaaSundar <swethaasundar@gmail.com>
2023-06-26 10:48:45 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./Components/Home/home";
+import Nav from "./Components/Nav/nav";
+import Experience from "./Components/Experience/experience";
+import Portfolio from "./Components/Portfolio/portfolio";
+import Contact from "./Components/Contact/contact";
+import Skills from "./Components/Skills/skills";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
+      <Nav />
+      <Experience />
+      <Skills />
+      <Portfolio />
+      <Contact />
     </div>
   );
 }
